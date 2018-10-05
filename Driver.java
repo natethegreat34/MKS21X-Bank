be8d9ae023b2e15377cd70b7c101a1a62c03e6cf
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-10-04 21:58:53 -0400

    Tester

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+  public static void main(STring[]args) {
+    BankAccount sonic = new BankAccount (12340, 1230, "hedgehog");
+    System.println ("Balance =" + sonic.getBalance());
+    System.println ("AccountID =" + sonic.getAccountID());
+    sonic.setPassword ("Lucario")
+    if( sonic.withdraw(1000) ){
+            println("Withdrawal success!");
+       }else{
+            println("Withdrawal failure");
+       }
+  }
+  )
